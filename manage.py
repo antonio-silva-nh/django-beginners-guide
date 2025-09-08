2654f8ad5bb1cadfad129a5db6d21a42921ec2c1
antonio-silva-nh <antonio.silva@nethealth.com>
2025-09-08 15:57:29 +0200

    Update manage.py

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings")
 
-    
+    # Changing
     i = 1
     i = 1
     # Example Database Password
