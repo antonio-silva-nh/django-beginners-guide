5067eabe76209476e6f352fce82223adeef260ff
antonio-silva-nh <antonio.silva@nethealth.com>
2025-09-08 15:59:33 +0200

    Update manage.py

--- manage.py
+++ manage.py
@@ -5,48 +5,7 @@ import sys
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings")
 
-    # Changing
-    i = 1
-    i = 1
-    # Example Database Password
-    db_password = "htwhtrhthtrhwr"
-    # Example AWS Secret Access Key
-    aws_secret_access_key = "wJalrXUtnFEMI/WSDFBTN/bPxRfiCYEXAMPLEKEY"
-    # Example AWS Access Key ID
-    aws_access_key_id = "AKIAIOSFODNN7EXAMPLE"
-    # Example GitHub Personal Access Token
-    github_token = "ghp_1234567890abcdefghijklmnopqrstuvwx"
-    # Example Slack Webhook URL
-    slack_webhook = "https://hooks.slack.com/services/T00000000/B00000000/XXXXXXXXXXXXXXXXXXXXXXXX"
-    # Example Google API Key
-    google_api_key = "AIzaSyA-EXAMPLEKEY1234567890abcdefghi"
-    # Example Stripe Secret Key
-    stripe_secret_key = "sk_test_4eC39HqLyjWDarjtT1zdp7dc"
-    # Example Twilio Auth Token
-    twilio_auth_token = "1234567890abcdef1234567890abcdef"
-    # Example Heroku API Key
-    heroku_api_key = "12345678-90ab-cdef-1234-567890abcdef"
-    # Example SendGrid API Key
-    sendgrid_api_key = "SG.xxxxxxxx.yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy"
-    # Example Facebook App Secret
-    facebook_app_secret = "1a2b3c4d5e6f7g8h9i0j1k2l3m4n5o6p"
-    # Example JWT Secret
-    jwt_secret = "supersecretjwtkey1234567890"
-    # Example Database URL
-    database_url = "postgres://myuser:mypassword@localhost:5432/mydatabase"
-    # Example SSH Private Key
-    # Example Private Key
-    private_key = """"
-    -----BEGIN RSA PRIVATE KEY-----
-    MIIEpAIBAAKCAQEA7v...
-    -----END RSA PRIVATE KEY-----
-    """
-    ssh_private_key = '''
-    -----BEGIN OPENSSH PRIVATE KEY-----
-    b3BlbnNzaC1rZXktdjEAAAAABG5vbmUAAAAEbm9uZQAAAAAAAAABAAABFwAAAAdzc2gtcn
-    NhAAAAAwEAAQAAAQEAw...
-    -----END OPENSSH PRIVATE KEY-----
-    '''
+    
 
     try:
         from django.core.management import execute_from_command_line
